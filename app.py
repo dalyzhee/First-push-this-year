6576c6b3af5dde7875e6c5b91dd2ffc81e490e12
dalyzhee <dalmasodhiambolevi@gmail.com>
2023-10-31 09:06:14 -0930

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+# this is a python program that reads data from a file, process it and sort the data and create a tuple
+# the finally save it in output file.
+
+# Step 1: Read data from input file
+input_data = []
+
+with open("input.txt", 'r') as file:
+    input_data = file.readlines()
+title = input_data[0]
+input_data = input_data[1:]
+data = []
+for line in input_data:
+    line = line.strip( )
+    if line:
+        name, age, score = line.split(',')
+            # print(name.strip( ), age.strip( ), score.strip( ))
+        person_data = (str(name.strip()), str(age.strip()), str(score.strip()))
+        # print(person_data)
+        data.append(person_data)
+# Sort the list by age in ascending order
+sorted_data = sorted(data, key=lambda x: x[1])
+# Print the sorted list
+with open("output.txt", 'w') as output:
+        output.write(title)
+        for item in sorted_data:
+            item_str = ', '.join(item)  # Convert the tuple to a comma-separated string
+            output.write(item_str + '\n')
+
+    
+# # print(title)
