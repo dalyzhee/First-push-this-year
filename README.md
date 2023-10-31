9055b1d11e597f8cd8541f4b26d9d37898a23a3d
dalyzhee <dalmasodhiambolevi@gmail.com>
2023-10-31 13:31:33 -0930

    Updating README.md file

---
+++ README.md
@@ -0,0 +1,21 @@
+# A simple python program
+
+This is a python program that reads data from a file, process it and sort the data and create a tuple the finally save it in output file.
+
+## APPLICATION FUNCTIONALITIES:
+
+1. Read data from a text file
+2. Process the data
+    - Create a tuple from the data read
+    - Sort the data based on age
+3. Store the processed data into a new output text file
+
+### Project image
+
+![alt text](screenshots/code.png)
+
+## How to run the program code
+- Open command prompt from a folder containing the python file and input.txt
+- run `python3 app.py`
+
+![alt text](screenshots/output.png)
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# this is a python program that reads data from a file, process it and sort the data and create a tuple
+# This is a python program that reads data from a file, process it and sort the data and create a tuple
 # the finally save it in output file.
 
 # Step 1: Read data from input file
@@ -9,20 +9,26 @@ with open("input.txt", 'r') as file:
 title = input_data[0]
 input_data = input_data[1:]
 data = []
+print("Unprocessed Data from input file.")
 for line in input_data:
     line = line.strip( )
     if line:
         name, age, score = line.split(',')
             # print(name.strip( ), age.strip( ), score.strip( ))
         person_data = (str(name.strip()), str(age.strip()), str(score.strip()))
-        # print(person_data)
+        
+        print(person_data)
         data.append(person_data)
 # Sort the list by age in ascending order
 sorted_data = sorted(data, key=lambda x: x[1])
 # Print the sorted list
+print()
+print("Processed Data.")
 with open("output.txt", 'w') as output:
         output.write(title)
         for item in sorted_data:
+            
+            print(item)
             item_str = ', '.join(item)  # Convert the tuple to a comma-separated string
             output.write(item_str + '\n')
 
